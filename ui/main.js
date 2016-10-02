8622a956ba8da747584d53a13ae3097c896403ac
Dilip <chinthada.saidilip@gmail.com>
2016-10-02 21:59:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,14 @@
 //Counter code
 
 var button = document.getElementById('counter');
+
 button.onclick = function(){
     
     // Create a request object 
     var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
-    request.onreadystatechange = function(){
+        request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200){
@@ -21,6 +22,6 @@ button.onclick = function(){
         };
         
         //Make the request
-        request.open('GET','http://coco98.imad.hasura-app.io/counter',true);
+        request.open('GET','http://sai-dilip-chinthada.imad.hasura-app.io/counter',true);
         request.send(null); 
 };
