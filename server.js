73217f51fdb16229c8e6e0542a47101a449a351b
Dilip <chinthada.saidilip@gmail.com>
2016-10-02 16:14:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,8 @@ app.get('/', function (req, res) {
 });
 
 
+
+
 app.get('/:articleName',function(req,res)
 {
     //articleName == article-One
@@ -97,6 +99,10 @@ app.get('/:articleName',function(req,res)
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
