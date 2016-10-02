255a231fe545e437d93c09dddafdec81e5396fe3
Dilip <chinthada.saidilip@gmail.com>
2016-10-02 22:47:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@ button.onclick = function(){
         };
         
         //Make the request
-        request.open('GET','http://sai-dilip-chinthada.imad.hasura-app.io/counter',true);
+        request.open(' GET','http://sai-dilip-chinthada.imad.hasura-app.io/counter',true);
         request.send(null); 
 };
 
@@ -31,10 +31,38 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to the server and send the name
+     
+    // Create a request object 
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+        request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+     //Capture a list of names and render it as a list
+        var names = ['name1','name2','name3','name4'];
+        var list = '';
+        for(var i=0;i<names.length;i++){
+            list += '<li>' + names[i] + '<li>';
+            }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+           
+            }
+        }
+        //Not done Yet
+    
+        };
+        
+        //Make the request
+        request.open(' GET','http://sai-dilip-chinthada.imad.hasura-app.io/submit-name?name='+name,true);
+        request.send(null);
+    
     
     //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
+    var names =request.responseText;
+    names = JSON.parse(names);
     var list = '';
     for(var i=0;i<names.length;i++){
         list += '<li>' + names[i] + '<li>';
