e08828e8a330fb89b568e473f72478cae5822c8f
Dilip <chinthada.saidilip@gmail.com>
2016-10-02 16:34:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+//move the image
+
+var img = document.getElementById('madi');
+
+img.onClick= function()
+{
+    img.style.marginLeft = '100px';
+};
