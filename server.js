52c2aea81f8432df5ddbe5cbd2ec9c0a2b576a11
Dilip <chinthada.saidilip@gmail.com>
2016-10-02 22:39:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get("/counter",function(req,res){
 var names = [];
 app.get('/submit-name',function(req,res){  //URL :/submit-name?name=xxxx
    //Get the name from the request
-   var name = req.params.name; //TODO
+   var name = req.query.name; //TODO
 
     names.push(name);
     //JSON: Javascript Object Notation
