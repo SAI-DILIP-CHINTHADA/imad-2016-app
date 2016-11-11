49ebb2845e3ba56aa124c366df469f8480921467
Dilip <chinthada.saidilip@gmail.com>
2016-11-11 22:15:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,5 @@
-//Counter code
 
-var button = document.getElementById('counter');
-
-button.onclick = function()
-{
-	//Create the request object
-	var request =new XMLHttpRequest();
-	
-
-
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function(){
-		if(request.readyState === XMLHttpRequest.DONE){
-			//Take some action
-			if(request.status === 200){
-			var counter = request.responseText;
-			var span = document.getElementById('count');
-			span.innerHTML = counter.toString();
-
-		}
-	}
-		//Not done yet
-	};
-	
-
-        //Make the request
-        request.open('GET','http://sai-dilip-chinthada.imad.hasura-app.io/counter',true);
-        request.send(null); 
-};
-//Submit name
+//Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
 	//Create a request object
@@ -38,23 +9,23 @@ submit.onclick = function(){
 		if(request.readyState === XMLHttpRequest.DONE){
 			//Take some action
 			if(request.status === 200){
-	var names = request.responseText;
-	names = JSON.parse(names);
-	var list='';
-	for(var i=0;i<names.length;i++)
-	{
-		list +='<li>'+ names[i] + '</li>';
-
-	}
-	var ul= document.getElementById('namelist');
-	ul.innerHTML = list;
-}
-}
+			    //Capture a list  of names and render it as a list
+			    console.log('user logged in');
+			    alert('Logged in sucessfully');
+        }   else if(request.status === 403){
+            alert('Username/password is incorrect');
+        }   else if(request.status === 500)
+        {
+            alert('Something went wrong on the server');
+        }
+    }
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-
-        request.open('GET','http://sai-dilip-chinthada.imad.hasura-app.io/submit-name?name=' + name,true);
-        request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+        request.open('POST','http://sai-dilip-chinthada.imad.hasura-app.io/login',true);
+        request.setRequestHeader('Content-Type','application/json'); 
+        request.send(JSON.stringify({username: username,password: password}));
 };
